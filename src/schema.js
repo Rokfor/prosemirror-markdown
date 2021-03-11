f911b5379a527a4b0152d7ac2742fefa7d507f02
Urs Hofer <u.hofer>
2021-03-11 19:37:39 +0100

    automatic build

--- src/schema.js
+++ src/schema.js
@@ -87,19 +87,20 @@ export const schema = new Schema({
     description_list: {
       content: "(description_term description_value)+",
       group: "block",
+      defining: true,
       parseDOM: [{tag: "dl"}],
       toDOM(node) { return ["dl", 0] }
     },
 
     description_term: {
-      content: "paragraph",
+      content: "inline*",
       defining: true,
       parseDOM: [{tag: "dt"}],
       toDOM() { return ["dt", 0] }
     },
 
     description_value: {
-      content: "paragraph",
+      content: "inline*",
       defining: true,
       parseDOM: [{tag: "dd"}],
       toDOM(node) { console.log('description_value', node); return ["dd", 0] }
